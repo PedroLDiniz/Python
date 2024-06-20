92649f51073a232c6854393bf7bb4c4fbde4bfa3
Pedro <pedrodinizctt@gmail.com>
2024-06-20 11:03:40 -0300

    HelloWorld

---
+++ app.py
@@ -0,0 +1,21 @@
+print('hello world')
+
+print ('1.  Cadastrar Restaurante')
+print ('2.  Listar Restaurante')
+print ('3.  Ativar Restaurante')
+print ('4.  Sair \n')
+
+opcao_escolhida = input('Escolha uma opção:')
+print('Você escolheu a opção', opcao_escolhida)
+
+print( 'python')
+
+nome = input
+idade = input
+
+print(f'meu nome é {nome} e tenho {idade} anos')
+print('a\n')
+print('l\n')
+print('u\n')
+print('r\n')
+print('a\n')
